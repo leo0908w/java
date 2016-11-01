68487f856407685cc7056351debfa5edaa9d4867
NickyChunWang <s91203@gmail.com>
2016-11-01 10:25:29 +0800

    2016-11-01 v1

---
+++ src/tw/org/iii/javatest/Will09.java
@@ -0,0 +1,18 @@
+package tw.org.iii.javatest;
+public class Will09 {
+    public static void main(String[] args){
+        int i = 0;
+        for (;;System.out.println("--------")){
+            //for (首次執行敘述句; 執行前的判斷; 執行後的敘述句)
+//            if (i >= 10){
+//
+//                System.out.println(i++);
+//                break;
+//            }
+        }
+    }
+    static void m1(){
+        System.out.println("test");
+    }
+
+}
---
+++ src/tw/org/iii/javatest/Will10.java
@@ -0,0 +1,15 @@
+package tw.org.iii.javatest;
+
+/**
+ * Created by iii-user on 2016/11/1.
+ */
+public class Will10 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            for (int j = 2; j < 6; j++) {
+                System.out.print( j + " x " + i + " = " + j * i + "\t");
+            }
+        System.out.println();
+        }
+    }
+}

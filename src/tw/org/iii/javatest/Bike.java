2f643cf39a44fb7a0c85fab5848acb399099aa22
NickyChunWang <s91203@gmail.com>
2016-11-01 16:30:39 +0800

    2016-11-01 v4

---
+++ src/tw/org/iii/javatest/Bike.java
@@ -0,0 +1,8 @@
+package tw.org.iii.javatest;
+public class Bike {
+    double speed;
+    void upSpeed() {
+    }
+    void downSpeed() {
+    }
+}
--- src/tw/org/iii/javatest/Pokerv2.java
+++ src/tw/org/iii/javatest/Pokerv2.java
@@ -1,4 +1,8 @@
 package tw.org.iii.javatest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Pokerv2 {
     public static void main(String[] args) {
         // 洗牌
@@ -27,12 +31,40 @@ public class Pokerv2 {
         for (int i=0; i<poker.length; i++) {
             players[i%4][i/4] = poker[i];
         }
-        for (int[] player : players) {
-            for (int v : player) {
-                System.out.print(v + "\t");
+        // 攤牌 (理牌)
+        String[] flower = {"黑桃", "愛心", "方塊", "梅花"};
+        String[] point = {"A", "2", "3", "4", "5", "6",
+                "7", "8", "9", "10", "J", "Q", "K"};
+        for (int[] player : players){
+            Arrays.sort(player);
+            for (int card : player){
+                System.out.print(flower[card/13] + point[card%13] + "  ");
+//                System.out.print(card + "\t");
             }
             System.out.println();
         }
-        // 攤牌 (理牌)
+//                int flower = card / 13;
+//                int point = card % 13;
+//                switch (flower) {
+//                    case 0: System.out.print("黑桃"); break;
+//                    case 1: System.out.print("愛心"); break;
+//                    case 2: System.out.print("方塊"); break;
+//                    case 3: System.out.print("梅花"); break;
+//                }
+//                switch (point) {
+//                    case 0: System.out.print("A\t"); break;
+//                    case 1: System.out.print("2\t"); break;
+//                    case 2: System.out.print("3\t"); break;
+//                    case 3: System.out.print("4\t"); break;
+//                    case 4: System.out.print("5\t"); break;
+//                    case 5: System.out.print("6\t"); break;
+//                    case 6: System.out.print("7\t"); break;
+//                    case 7: System.out.print("8\t"); break;
+//                    case 8: System.out.print("9\t"); break;
+//                    case 9: System.out.print("10\t"); break;
+//                    case 10: System.out.print("J\t"); break;
+//                    case 11: System.out.print("Q\t"); break;
+//                    case 12: System.out.print("K\t"); break;
+//                }
     }
 }

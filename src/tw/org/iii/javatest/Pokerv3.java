96acced5736176d19ff572320999534cc2917153
NickyChunWang <s91203@gmail.com>
2016-11-02 16:41:14 +0800

    2016-11-02 v3

--- src/tw/org/iii/javatest/Pokerv3.java
+++ src/tw/org/iii/javatest/Pokerv3.java
@@ -1,6 +1,25 @@
 package tw.org.iii.javatest;
 public class Pokerv3 {
     public static void main(String[] args) {
-
+        int[] poker = new int[52];
+        int temp;
+        for (int j=0; j<poker.length; j++) {
+            poker[j] = j;
+            System.out.print(poker[j] + ",\t");
+        }
+        System.out.println();
+        System.out.println("----------");
+        for (int i=1; i<=poker.length; i++) {
+            int a = poker.length-i;
+            int change = (int)(Math.random() * (poker.length-i));
+            temp = poker[change];
+//            System.out.print(temp + "\t");
+            poker[change] = poker[a];
+//            System.out.print(temp2 + "\t");
+            poker[a] = temp;
+        }
+        for (int k=0; k<poker.length; k++) {
+            System.out.print(poker[k] + ",\t");
+        }
     }
 }
--- src/tw/org/iii/javatest/hw01.java
+++ src/tw/org/iii/javatest/hw01.java
@@ -1,7 +1,17 @@
 package tw.org.iii.javatest;
-
-/**
- * Created by iii-user on 2016/11/1.
- */
 public class hw01 {
+    public static void main(String[] args) {
+        int temp = 0;
+        for (int i=2; i<100; i++) {
+            for (int j=2; j<(i+1); j++) {
+                if ( i%j == 0 ) {
+                    temp++;
+                }
+            }
+            if (temp == 1) {
+                System.out.print(i +",\t");
+            }
+            temp = 0;
+        }
+    }
 }

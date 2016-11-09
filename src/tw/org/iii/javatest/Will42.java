595ac2777d097de91885471d96a1b10ecce19655
NickyChunWang <s91203@gmail.com>
2016-11-09 16:12:56 +0800

    2016-11-09 v1

--- src/tw/org/iii/javatest/Will42.java
+++ src/tw/org/iii/javatest/Will42.java
@@ -1,18 +1,36 @@
 package tw.org.iii.javatest;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 
+// client: send file
 public class Will42 {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         try {
-            Socket socket = new Socket(InetAddress.getByName("10.2.24.123"), 9999);
+            Socket socket = new Socket(InetAddress.getByName("10.2.24.106"), 9999);
+            FileInputStream fin = new FileInputStream("dir1/test123.jpg");
+            OutputStream out = socket.getOutputStream();
+            int b;
+            while ((b = fin.read()) != -1) {
+                out.write(b);
+            }
+            fin.close();
+            out.flush();
+            out.close();
+
+//            BufferedOutputStream bout = new BufferedOutputStream(
+//                    socket.getOutputStream());
+//            bout.write("你好阿!!\n".getBytes());
+//            bout.flush();
+//            bout.close();
 
             socket.close();
             System.out.println("OK");
-        } catch (IOException ee) {
-            ee.printStackTrace();
+            System.out.println(System.currentTimeMillis() - start);
+        } catch (Exception ee) {
+            System.out.println("Client Exception : " + ee.toString());
         }
     }
 }
--- src/tw/org/iii/javatest/Will43.java
+++ src/tw/org/iii/javatest/Will43.java
@@ -1,16 +1,42 @@
 package tw.org.iii.javatest;
 
-import java.io.IOException;
-import java.net.ServerSocket;
+import sun.awt.InputMethodSupport;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+// server
 public class Will43 {
     public static void main(String[] args) {
         try {
             ServerSocket server = new ServerSocket(9999);
-            server.accept();
+            System.out.println("listen.....");
+            Socket socket = server.accept();
+
+            String urip = socket.getInetAddress().getHostAddress();
+            System.out.println("ip : " + urip);
+
+            FileOutputStream fout = new FileOutputStream("upload/" + urip + ".jpg");
+            InputStream in = socket.getInputStream();
+            int b;
+            while ((b = in.read()) != -1) {
+                fout.write(b);
+            }
+            in.close();
+
+//            BufferedReader br = new BufferedReader(
+//                    new InputStreamReader(socket.getInputStream()));
+//
+//            String line;
+//            while ((line = br.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//            br.close();
+
             server.close();
-            System.out.println("server OK");
-        } catch (IOException e) {
+            System.out.println("Server OK");
+        } catch (Exception e) {
 
         }
 
